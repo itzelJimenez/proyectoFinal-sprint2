7e0d2b6853d9e9a86d317490e912c3423068369d
Itzel <itzeljimenez.do@gmail.com>
2017-03-29 11:38:48 +0000

    botones de borrar bien conectados

--- assets/js/js.js
+++ assets/js/js.js
@@ -18,7 +18,7 @@ function crearLista(Lista){
 		var nodo = document.createTextNode(nombreDeNuevaLista);
 		nuevaLista.appendChild(nodo);
 			
-		var aquiHareOtraLista = document.getElementById("nombre");
+		var aquiHareOtraLista = document.getElementById("listasNuevas");
 		aquiHareOtraLista.appendChild(nuevaLista);
 		
 		var formulario = document.createElement("input");
@@ -33,11 +33,33 @@ function crearLista(Lista){
 			var elementoLi = document.createElement("LI");
 			var nombreElemento = document.getElementById("nombre").value;
 			var nodo = document.createTextNode(nombreElemento);
-			elementoLi.appendChild(nodo);
+				elementoLi.appendChild(nodo);
+				nuevaLista.appendChild(elementoLi);
+			//Agrega todo el elemento Li a la Lista
 			nuevaLista.appendChild(elementoLi);
+
+			//Agregar botón de borrar a cada elemento de los elementos
+			var botonBorrar = document.createElement('input');
+				botonBorrar.type = "button";
+				botonBorrar.value = "Eliminar tarea";
+			botonBorrar.onclick = function(){
+				alert("funciona chidoliro");
+				nuevaLista.removeChild(elementoLi);
+				nuevaLista.removeChild(botonBorrar);
+			}
+			
+			
+			nuevaLista.appendChild(botonBorrar);
 		}
 		nuevaLista.appendChild(boton);
-		
+		var botonBorrarLista = document.createElement("input");
+		botonBorrarLista.type = "button";
+		botonBorrarLista.value = "Eliminar Lista";
+		botonBorrarLista.onclick = function(){
+			alert("ya programa lo de la borrada")
+		}
+		nuevaLista.appendChild(botonBorrarLista);
+
 		
 }
 
