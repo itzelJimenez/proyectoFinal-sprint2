cabc3f63978413323a324e32f533d9a4fd606beb
Itzel <itzeljimenez.do@gmail.com>
2017-03-29 13:46:24 +0000

    Versión Final, código comentado

--- assets/js/js.js
+++ assets/js/js.js
@@ -33,7 +33,7 @@ function crearLista(Lista){
 	//ormulario.id= "nombre";
 		nuevaLista.appendChild(formulario);
 */
-
+	//Agrega botón para crear un nuevo elemento en la lista
 		var boton = document.createElement("input");
 		boton.type = "button";
 		boton.value= "Agregar elemento";
@@ -42,11 +42,16 @@ function crearLista(Lista){
 			var nombreElemento = prompt("Que tarea quieres agregar?");
 			var nodo = document.createTextNode(nombreElemento);
 				elementoLi.appendChild(nodo);
-				nuevaLista.appendChild(elementoLi);
+			
 			//Agrega todo el elemento Li a la Lista
-			nuevaLista.appendChild(elementoLi);
+				nuevaLista.appendChild(elementoLi);
 
-			//Agrega botón de borrar a cada uno de los elementos
+			//Intento de crear un separador dinámico
+			var crearSeparador = document.createElement('HR');
+		
+			nuevaLista.appendChild(crearSeparador);
+
+			//Agrega botón de borrar elemento a cada uno de los elementos, borra los elementos y borra el botón. 
 			var botonBorrar = document.createElement('input');
 				botonBorrar.type = "button";
 				botonBorrar.value = "Eliminar tarea";
@@ -57,8 +62,9 @@ function crearLista(Lista){
 			//Agrega el elemento input botón como hijo del elemento UL, variable NUEVA LISTA
 			nuevaLista.appendChild(botonBorrar);
 		}
+		//Agregar boton como nodo de nueva lista
 		nuevaLista.appendChild(boton);
-		//Botón que borra toda la lista
+		//Botón que borra toda la lista y también el botón.
 		var botonBorrarLista = document.createElement("input");
 		botonBorrarLista.type = "button";
 		botonBorrarLista.value = "Eliminar Lista";
--- reformulandocodigo.js
+++ reformulandocodigo.js
@@ -1,1 +1,66 @@
-function crear
+//Intento de reformulación de código pero ya lo probé y no funciona, debo modularizar mejor.
+
+function borrarElemento(){
+		nuevaLista.removeChild(elementoLi);
+		nuevaLista.removeChild(botonBorrar);
+}
+
+function removerLista(){
+		aquiHareOtraLista.removeChild(nuevaLista);
+		aquiHareOtraLista.removeChild(botonBorrarLista);
+}
+function agregarElemento(){
+			var elementoLi = document.createElement("LI");
+			var nombreElemento = prompt("¿Qué tarea quieres agregar?");
+			var nodo = document.createTextNode(nombreElemento);
+				elementoLi.appendChild(nodo);
+				nuevaLista.appendChild(elementoLi);
+			//Agrega todo el elemento Li a la Lista
+			nuevaLista.appendChild(elementoLi);
+
+			//Agrega botón de borrar a cada uno de los elementos
+			var botonBorrar = document.createElement('input');
+				botonBorrar.type = "button";
+				botonBorrar.value = "Eliminar tarea";
+			botonBorrar.onclick = borrarElemento();
+			//Agrega el elemento input botón como hijo del elemento UL, variable NUEVA LISTA
+			nuevaLista.appendChild(botonBorrar);
+}
+function crearLista(Lista){
+	//CREA ELEMENTO UL EN EL HTML
+		var nuevaLista = document.createElement('UL');
+	//OBTIENE EL NOMBRE DE LA LISTA QUE SE VA A ASIGNAR AL UL
+		var nombreDeNuevaLista =document.getElementById("nombreNuevaLista").value;
+	//CONVIERTE A TEXTO NODO EL NOMBRE DE LA NUEVA LISTA
+		var nodo = document.createTextNode(nombreDeNuevaLista);
+	//SE ASIGNA EL NOMBRE DE LA NUEVA LISTA AL ELEMENTO UL COMO CONTENIDO
+		nuevaLista.appendChild(nodo);
+	//OBTIENE EL LUGAR EN DONDE SE CREARÁ UN ELEMENTO LI NUEVO
+		var aquiHareOtraLista = document.getElementById("listasNuevas");
+	//ASIGNA LA NUEVA LISTA COMO ELEMENTO HIJO DE UN SECTION CON ID: LISTAS NUEVAS
+		aquiHareOtraLista.appendChild(nuevaLista);
+	/*
+	//Crea input de tipo texto, para agregar tareas 
+		var formulario = document.createElement("input");
+		formulario.type = "text";
+	//ormulario.id= "nombre";
+		nuevaLista.appendChild(formulario);
+*/
+
+		var boton = document.createElement("input");
+		boton.type = "button";
+		boton.value= "Agregar elemento";
+		boton.onclick = agregarElemento();
+		nuevaLista.appendChild(boton);
+		//Botón que borra toda la lista
+		var botonBorrarLista = document.createElement("input");
+		botonBorrarLista.type = "button";
+		botonBorrarLista.value = "Eliminar Lista";
+		botonBorrarLista.onclick = removerLista();
+		}
+		nuevaLista.appendChild(botonBorrarLista);
+
+		
+}
+
+
